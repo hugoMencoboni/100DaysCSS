52a575c326cc2dcca0d494c123e116fa206bd0fd
hugo.M <hugo.mencoboni@gmail.com>
2020-11-14 21:51:01 -0500

    enter password: main features

--- src/Components/ShowRoom/EnterPassword/EnterPassword.js
+++ src/Components/ShowRoom/EnterPassword/EnterPassword.js
@@ -3,32 +3,28 @@ import  './EnterPassword.scoped.scss';
 
 class EnterPassword extends Component {
     state = {
-      info: "lalala",
       showSecret: false,
     }
         
     inputChange = (event) => {
         if(event?.target?.value === "password") {
-            this.showSecret();
+            this.setState({ showSecret: !this.state.showSecret });
         }
-    }
-
-    showSecret = () => {
-        this.setState({ showSecret: !this.state.showSecret });
     }
   
     render() {
-        let a = <div className="secret box">MD5-SU3-CX8</div>;
-        let classSecret = this.state.showSecret ? "rotatedBox show" : "rotatedBox";
+      let classSecret = this.state.showSecret ? "rotatedBox show" : "rotatedBox";
 
       return (
         <div className="challenge">
-            <div className="container">
-                <div className={classSecret}>
-                    <input type="text" className="box" placeholder='Enter "password"' onChange={this.inputChange}/>
-                    <div className="secret-text">secret</div>
+            <div className="container">      
+                <div className="rotatedBox">
+                    <div className="secret box">MD5-SU3-CX8</div>
                 </div>
-                <button onClick={this.showSecret}>test annimation</button>
+                <div className={classSecret}>
+                    <div className="secret-text box">Secret Key</div>
+                    <input type="text" className="box" placeholder='Enter "password"' onChange={this.inputChange}/>
+                </div>
             </div>
         </div>
       );
