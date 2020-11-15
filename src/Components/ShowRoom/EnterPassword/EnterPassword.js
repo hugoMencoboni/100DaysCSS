18722f1023fd59231bcbfcb79849d95d28818eba
hugo.M <hugo.mencoboni@gmail.com>
2020-11-14 19:54:58 -0500

    animation

--- src/Components/ShowRoom/EnterPassword/EnterPassword.js
+++ src/Components/ShowRoom/EnterPassword/EnterPassword.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import  './EnterPassword.scoped.scss';
 
-const EnterPassword = props => {
-    return (
+class EnterPassword extends Component {
+    state = {
+      info: "lalala",
+      showSecret: false,
+    }
+        
+    inputChange = (event) => {
+        if(event?.target?.value === "password") {
+            this.showSecret();
+        }
+    }
+
+    showSecret = () => {
+        this.setState({ showSecret: !this.state.showSecret });
+    }
+  
+    render() {
+        let a = <div className="secret box">MD5-SU3-CX8</div>;
+        let classSecret = this.state.showSecret ? "rotatedBox show" : "rotatedBox";
+
+      return (
         <div className="challenge">
             <div className="container">
-                <input type="text" className="box" placeholder='Enter "password"' />
-                <div className="secret box">MD5-SU3-CX8</div>
-                <div className="secret-text">secret</div>
-                <button>test annimation</button>
+                <div className={classSecret}>
+                    <input type="text" className="box" placeholder='Enter "password"' onChange={this.inputChange}/>
+                    <div className="secret-text">secret</div>
+                </div>
+                <button onClick={this.showSecret}>test annimation</button>
             </div>
         </div>
-    );
-}
+      );
+    }
+  }
 
 export default EnterPassword;
