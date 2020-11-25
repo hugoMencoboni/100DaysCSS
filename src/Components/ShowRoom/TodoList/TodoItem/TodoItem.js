6e40458f387152225786605f15ffeff125c47132
hugo.M <hugo.mencoboni@gmail.com>
2020-11-24 19:17:48 -0500

    Fix: todolist checked animation

--- src/Components/ShowRoom/TodoList/TodoItem/TodoItem.js
+++ src/Components/ShowRoom/TodoList/TodoItem/TodoItem.js
@@ -6,11 +6,11 @@ const TodoItem = props => {
         <label className="todo">
             <input type="checkbox" onClick={props.todoChecked}/>
             <div className="label">{props.label}</div>
-            <div className="fakeCheckbox-unchecked"></div>
             <div className="fakeCheckbox-checked">
                 <div></div>
                 <div></div>
             </div>
+            <div className="fakeCheckbox-unchecked"></div>
         </label>
     );
 }
